942a09a8a9e7d906f5a4ce69599f1f406925ca8f
pushrefresh <rossi@pushrefresh.com>
2025-01-17 17:07:50 -0600

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -14,22 +14,24 @@ import { MobileNav } from '@/components/mobile-nav'
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center">
-        <MainNav />
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button
-              variant="ghost"
-              className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
-            >
-              <Menu className="h-6 w-6" />
-              <span className="sr-only">Toggle menu</span>
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="left" className="pr-0">
-            <MobileNav />
-          </SheetContent>
-        </Sheet>
+      <div className="container flex h-14 items-center justify-between">
+        <div className="flex items-center gap-6">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="ghost"
+                className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+              >
+                <Menu className="h-6 w-6" />
+                <span className="sr-only">Toggle menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="pr-0">
+              <MobileNav />
+            </SheetContent>
+          </Sheet>
+          <MainNav />
+        </div>
       </div>
     </header>
   )
