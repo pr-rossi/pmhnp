4a1a0e5f9ed65e9a63ef56f8bca0c5b851e6fc78
pushrefresh <rossi@pushrefresh.com>
2025-01-18 11:11:39 -0600

    a

--- components/providers/session-provider.tsx
+++ components/providers/session-provider.tsx
@@ -4,7 +4,11 @@ import { SessionProvider as NextAuthSessionProvider } from "next-auth/react"
 
 export function SessionProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextAuthSessionProvider refetchInterval={0} refetchOnWindowFocus={true}>
+    <NextAuthSessionProvider 
+      refetchInterval={0} 
+      refetchOnWindowFocus={true}
+      refetchWhenOffline={false}
+    >
       {children}
     </NextAuthSessionProvider>
   )
