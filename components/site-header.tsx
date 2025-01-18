3123eb07957ef9469426ad76230cf1c8bc6b4870
pushrefresh <rossi@pushrefresh.com>
2025-01-18 11:09:47 -0600

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,15 +1,18 @@
+"use client"
+
 import Link from 'next/link'
-import { getServerSession } from 'next-auth/next'
+import { useSession } from "next-auth/react"
 import { MainNav } from '@/components/main-nav'
 import { ThemeToggle } from '@/components/theme-toggle'
 
-export async function SiteHeader() {
-  const session = await getServerSession()
+export function SiteHeader() {
+  const { data: session, status } = useSession()
+  const isAuthenticated = status === "authenticated"
 
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav isAuthenticated={!!session} />
+        <MainNav isAuthenticated={isAuthenticated} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
             <ThemeToggle />
