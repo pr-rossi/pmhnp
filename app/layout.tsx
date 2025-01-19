6b5fa36698acc501a964a367ed26990c24b0d00a
pushrefresh <rossi@pushrefresh.com>
2025-01-18 18:04:28 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
             >
               <div className="relative flex min-h-screen flex-col">
                 <SiteHeader />
-                <div className="flex-1">{children}</div>
+                <main className="container mx-auto flex-1 px-4">{children}</main>
                 <ThemeToggle />
                 <Toaster richColors closeButton position="top-center" />
               </div>
